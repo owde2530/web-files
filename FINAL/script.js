e428b6312b1aeaffd93cefa6026800a6125a9afb
owde2530 <70664682+owde2530@users.noreply.github.com>
2021-12-09 17:33:07 -0700

    changes 3 12/9/21

--- FINAL/script.js
+++ FINAL/script.js
@@ -1,15 +1,24 @@
-document.getElementsByClassName("backbutton").addEventListener("hover", buttonGrow());
-document.getElementsByClassName("backbutton").addEventListener("click", buttonClick());
-document.getElementsByClassName("mpbutton").addEventListener("hover",buttonGrow());
-document.getElementsByClassName("mpbutton").addEventListener("click",buttonClick());
+for (i=0;i < document.getElementsByClassName("mpbutton").length; i++)
+{
+  document.getElementsByClassName("mpbutton")[i].addEventListener("mouseover",buttonGrow);
+  document.getElementsByClassName("mpbutton")[i].addEventListener("click",buttonClick);
+  console.log("hi1");
+}
+
+document.getElementById("backbutton1").addEventListener("mouseover", buttonGrow);
+document.getElementById("backbutton1").addEventListener("click", buttonClick);
 
 function buttonGrow() {
-  document.getElementsByClassName("backbutton").style.color = "red";
-  document.getElementsByClassName("mpbutton").style.color = "red";
-  console.log("hi");
+  document.getElementById("backbutton1").style.backgroundColor = "red";
+  for (i=0;i < document.getElementsByClassName("mpbutton").length; i++)
+  {
+    document.getElementsByClassName("mpbutton")[i].style.color = "red";
+    console.log("hi");
+  }
+  // console.log("hi");
 }
 function buttonClick() {
-  document.getElementsByClassName("backbutton").style.color = "green";
-  document.getElementsByClassName("mpbutton").style.color = "green";
+  document.getElementById("backbutton1").style.color = "green";
+  document.getElementsByClassName("mpbutton")[i].style.color = "green";
   console.log("hi");
 }
