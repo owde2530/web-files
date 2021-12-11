b6e69fe18b2a0d7684cf3cbfba3e44cf1768b779
owde2530 <70664682+owde2530@users.noreply.github.com>
2021-12-10 19:39:13 -0700

    changes 12/10/21

---
+++ FINALS/script.js
@@ -0,0 +1,5 @@
+document.getElementById("backbutton1").addEventListener("mouseover", bbButtonGrow);
+
+function bbButtonGrow() {
+  document.getElementById("backbutton1").style.backgroundColor = "black";
+}
--- FINALV/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("backbutton1").addEventListener("mouseover", bbButtonGrow);
-
-function bbButtonGrow() {
-  document.getElementById("backbutton1").style.backgroundColor = "black";
-}
