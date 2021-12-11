68c8c4dbe58dd6eedc2759ec928b503d33d6a41e
owde2530 <70664682+owde2530@users.noreply.github.com>
2021-12-10 19:10:02 -0700

    changes 5 12/10/21

--- FINAL/script.js
+++ FINAL/script.js
@@ -1,24 +1,5 @@
-for (i=0;i < document.getElementsByClassName("mpbutton").length; i++)
-{
-  document.getElementsByClassName("mpbutton")[i].addEventListener("mouseover",buttonGrow);
-  document.getElementsByClassName("mpbutton")[i].addEventListener("click",buttonClick);
-  console.log("hi1");
-}
+document.getElementById("backbutton1").addEventListener("mouseover", bbButtonGrow);
 
-document.getElementById("backbutton1").addEventListener("mouseover", buttonGrow);
-document.getElementById("backbutton1").addEventListener("click", buttonClick);
-
-function buttonGrow() {
-  document.getElementById("backbutton1").style.backgroundColor = "red";
-  for (i=0;i < document.getElementsByClassName("mpbutton").length; i++)
-  {
-    document.getElementsByClassName("mpbutton")[i].style.color = "red";
-    console.log("hi");
-  }
-  // console.log("hi");
-}
-function buttonClick() {
-  document.getElementById("backbutton1").style.color = "green";
-  document.getElementsByClassName("mpbutton")[i].style.color = "green";
-  console.log("hi");
+function bbButtonGrow() {
+  document.getElementById("backbutton1").style.backgroundColor = "black";
 }
